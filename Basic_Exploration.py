94e8aa5e57d3be5126bcf633e03c693c617ccffb
Parth4786 <80087434+Parth4786@users.noreply.github.com>
2022-11-27 16:13:44 +0530

    First Commit

---
+++ Basic_Exploration.py
@@ -0,0 +1,32 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pandas as pd"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "melbourne_file_path = 'MELBOURNE_HOUSE_PRICES_LESS.csv'\n",
+    "melbourne_data = pd.read_csv(melbourne_file_path)\n",
+    "melbourne_data.describe()"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
